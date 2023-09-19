5bd0ecfc161ad5cda4aaace8f96da328dfa469e8
sheryel <sheryel57@gmail.com>
2023-09-19 13:40:52 +0800

    Initial Commit

---
+++ thigy/thigyApp.swift
@@ -0,0 +1,17 @@
+//
+//  thigyApp.swift
+//  thigy
+//
+//  Created by sheryel on 19/9/23.
+//
+
+import SwiftUI
+
+@main
+struct thigyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
