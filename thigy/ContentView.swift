06fd95c38612b0b37079e8c926bd0053f791ab5a
sheryel <sheryel57@gmail.com>
2023-09-21 12:35:03 +0800

    add lesson 1 yay

--- thigy/ContentView.swift
+++ thigy/ContentView.swift
@@ -9,16 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Divider()
+                Spacer()
+                Text("lesson 1!!")
+                    .font(.title2)
+                List {
+                    NavigationLink {
+                        
+                    } label: {
+                        Text("printing and commenting")
+                    }
+                    NavigationLink {
+                        
+                    } label: {
+                        Text("variables, constants and data types")
+                    }
+                    NavigationLink {
+                        
+                    } label: {
+                        Text("conditionals")
+                    }
+                    NavigationLink {
+                        
+                    } label: {
+                        Text("operators")
+                    }
+                    NavigationLink {
+                        
+                    } label: {
+                        Text("functions")
+                    }
+                }
+                .listStyle(PlainListStyle())
+                Divider()
+                Text("lesson 2!!")
+                    .font(.title)
+                List {
+                    
+                }
+                .listStyle(PlainListStyle())
+            }
+            .navigationTitle("swift documentation")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
