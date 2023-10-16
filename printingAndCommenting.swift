b396f945188cdb858a710b170afbf0ec9f6bb6f6
sheryel <sheryel57@gmail.com>
2023-10-16 22:58:38 +0800

    added all the lessons and made file for lesson 1a

---
+++ printingAndCommenting.swift
@@ -0,0 +1,18 @@
+//
+//  printingAndCommenting.swift
+//  thigy
+//
+//  Created by sheryel on 16/10/23.
+//
+
+import SwiftUI
+
+struct printingAndCommenting: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    printingAndCommenting()
+}
--- thigy/ContentView.swift
+++ thigy/ContentView.swift
@@ -8,33 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isLesson1Visible = true
-    
     var body: some View {
         NavigationView {
             VStack {
                 List {
                     // lesson 1
                     Section(header: Text("lesson 1")) {
-                        if isLesson1Visible {
-                            NavigationLink ("a) printing and commenting") {
-                                
-                            }
-                            NavigationLink ("b) variables, constants and data types") {
-                                
-                            }
-                            NavigationLink ("c) conditionals") {
-                                
-                            }
-                            NavigationLink ("d) operators") {
-                                
-                            }
-                            NavigationLink ("e) functions") {
-                                
-                            }
-                            .navigationTitle("lesson 1")
+                        NavigationLink ("printing and commenting") {
+                            printingAndCommenting()
                         }
+                        NavigationLink ("variables, constants and data types") {
+                            
+                        }
+                        NavigationLink ("conditionals") {
+                            
+                        }
+                        NavigationLink ("operators") {
+                            
+                        }
+                        NavigationLink ("functions") {
+                            
+                        }
+                        .navigationTitle("lesson 1")
                     }
+                    
                     // lesson 2
                     Section(header: Text("lesson 2")) {
                         NavigationLink("functions") {
@@ -49,6 +46,83 @@ struct ContentView: View {
                         NavigationLink("filter map reduce") {
                             
                         }
+                        .navigationTitle("lesson 2")
+                    }
+                    
+                    // lesson 3
+                    Section(header: Text("lesson 3")) {
+                        NavigationLink("map and reduce again") {
+                            
+                        }
+                        NavigationLink("optionals") {
+                            
+                        }
+                        NavigationLink("structs") {
+                            
+                        }
+                        NavigationLink("computed properties") {
+                            
+                        }
+                        NavigationLink("enums") {
+                            
+                        }
+                        .navigationTitle("lesson 3")
+                    }
+                    
+                    // lesson 4
+                    Section(header: Text("lesson 4")) {
+                        NavigationLink("1") {
+                            
+                        }
+                        .navigationTitle("lesson 4")
+                    }
+                    
+                    // lesson 5
+                    Section(header: Text("lesson 5")) {
+                        NavigationLink("1") {
+                            
+                        }
+                        .navigationTitle("lesson 5")
+                    }
+                    
+                    // lesson 6
+                    Section(header: Text("lesson 6")) {
+                        NavigationLink("1") {
+                            
+                        }
+                        .navigationTitle("lesson 6")
+                    }
+                    
+                    // lesson 7
+                    Section(header: Text("lesson 7")) {
+                        NavigationLink("1") {
+                            
+                        }
+                        .navigationTitle("lesson 7")
+                    }
+                    
+                    // lesson 8
+                    Section(header: Text("lesson 8")) {
+                        NavigationLink("1") {
+                            
+                        }
+                        .navigationTitle("lesson 8")
+                    }
+                    
+                    // lesson 9
+                    Section(header: Text("lesson 9")) {
+                        NavigationLink("1") {
+                            
+                        }
+                        .navigationTitle("lesson 9")
+                    }
+                    
+                    // lesson 10
+                    Section(header: Text("lesson 10")) {
+                        NavigationLink("1") {
+                            
+                        }
+                        .navigationTitle("lesson 10")
                     }
                 }
             }
